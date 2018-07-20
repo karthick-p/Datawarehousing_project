434730a8b05cf141aff63a1ee3d036c6957b3311
karthick-p <kr688977@dal.ca>
2018-07-19 21:18:53 -0300

    multivarient analysis
    multivarient analysis with matplot

--- Loading/pandas_tuning_dataset.py
+++ Loading/pandas_tuning_dataset.py
@@ -7,35 +7,33 @@ from sklearn.model_selection import train_test_split
 from sklearn import linear_model
 
 
-df = pd.read_csv("onemonth.csv", delimiter = ",", encoding="utf-8")
+df = pd.read_csv("trial.csv", delimiter = ",", encoding="utf-8")
 df=df.dropna()
 
 train, test = train_test_split(df, test_size=0.2)
 
 train_x = train.drop(columns=['Supply_MW','datetime'])
-train_y = train.drop(columns=['datetime','Temp'])
+train_y = train.drop(columns=['datetime','Temp','Humidity','wind_speed'])
 
 #this creates the linear regeression model
 
 lm = linear_model.LinearRegression()
 lm.fit(train_x,train_y)
 
-print(df)
 
 test_x = test.drop(columns=['Supply_MW','datetime'])
-test_y = test.drop(columns=['datetime','Temp'])
+test_y = test.drop(columns=['datetime','Temp','Humidity','wind_speed'])
 
 #test_y.to_csv("actual.csv", sep='\t', encoding='utf-8')
+test_x1 = test['Temp']
+test_x2 = test['Humidity']
+test_x3 = test['wind_speed']
 
 
+print(test_x1)
 pred_y = lm.predict(test_x)
 #pred_y.to_csv("predicted.csv", sep='\t', encoding='utf-8')
 
-
-print("hi")
-print(test_y)
-print("check")
-print(pred_y)
 
 # The coefficients
 print('Coefficients: \n', lm.coef_)
@@ -46,8 +44,15 @@ print("Mean squared error: %.2f"
 print('Variance score: %.2f' % r2_score(test_y, pred_y))
 
 # Plot outputs
-plt.scatter(test_x, test_y,  color='black')
-plt.plot(test_x, pred_y, color='blue', linewidth=3)
+plt.scatter(test_x1, test_y,  color='black')
+plt.scatter(test_x2, test_y,  color='black')
+plt.scatter(test_x3, test_y,  color='black')
+
+
+plt.plot(test_x1, pred_y, color='blue', linewidth=3)
+plt.plot(test_x2, pred_y, color='blue', linewidth=3)
+plt.plot(test_x3, pred_y, color='blue', linewidth=3)
+
 
 plt.xticks(())
 plt.yticks(())
